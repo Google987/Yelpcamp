cd69a7e6005265b7952e600c8e26faac90260776
root <root@localhost.localdomain>
2018-06-06 09:56:38 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var passLocalMongoose = require("passport-local-mongoose");
+
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+UserSchema.plugin(passLocalMongoose);
+module.exports = mongoose.model("User", UserSchema);
